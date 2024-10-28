beefed8481ee12c61224768525538533e7aa6e21
YUSUF EMRE DENİZ <yusufemredenix@outlook.com>
2024-10-28 11:27:55 +0300

    Resim Yükleme İşlemleri

--- Kurumsal_Web11/Controllers/KimlikController.cs
+++ Kurumsal_Web11/Controllers/KimlikController.cs
@@ -2,8 +2,10 @@
 using Kurumsal_Web11.Models.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace Kurumsal_Web11.Controllers
@@ -33,6 +35,28 @@ namespace Kurumsal_Web11.Controllers
             if (ModelState.IsValid)
             {
                 var k=db.Kimlik.Where(x=>x.KimlikId == id).SingleOrDefault();
+
+                if(LogoURL != null)
+                {
+                    if (System.IO.File.Exists(Server.MapPath(k.LogoURL)))
+                    {
+                        System.IO.File.Delete(Server.MapPath(k.LogoURL));
+                    }
+                    WebImage img = new WebImage(LogoURL.InputStream);
+                    FileInfo imginfo = new FileInfo(LogoURL.FileName);
+
+                    string logoname = LogoURL.FileName + imginfo.Extension;
+                    img.Resize(300, 200);
+                    img.Save("~/Uploads/Kimlik/" + logoname);
+
+                    k.LogoURL = "/Uploads/Kimlik/" + logoname;
+                }
+                k.Title= kimlik.Title;
+                k.Description= kimlik.Description;
+                k.Keywords= kimlik.Keywords;
+                k.Unvan= kimlik.Unvan;
+                db.SaveChanges();
+                return RedirectToAction("Index");//controller'ın Index'e gitsin
             }
 
             return View();
