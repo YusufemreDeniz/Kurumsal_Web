13d438173211657a682ead347f4518ed01c05e70
YUSUF EMRE DENİZ <yusufemredenix@outlook.com>
2024-11-08 15:15:59 +0300

    İletişim ve css eklendi

---
+++ Kurumsal_Web11/Migrations/202411080629035_UpdateDatabase.cs
@@ -0,0 +1,18 @@
+﻿namespace Kurumsal_Web11.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Hakkimizda", "Aciklama", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Hakkimizda", "Aciklama", c => c.String(nullable: false));
+        }
+    }
+}
