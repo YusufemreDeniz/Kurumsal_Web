7265e8abbdabe3ca54608b896139a818c7770653
YUSUF EMRE DENİZ <yusufemredenix@outlook.com>
2024-11-13 23:21:58 +0300

    Hizmetler eklendi

---
+++ Kurumsal_Web11/Controllers/HizmetController.cs
@@ -0,0 +1,19 @@
+﻿using Kurumsal_Web11.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kurumsal_Web11.Controllers
+{
+    public class HizmetController : Controller
+    {
+        private KurumsalDBContext db = new KurumsalDBContext();
+        // GET: Hizmet
+        public ActionResult Index()
+        {
+            return View(db.Hizmet.ToList());
+        }
+    }
+}
