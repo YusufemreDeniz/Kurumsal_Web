3a816aa90fec94a7e69fa82e15eef37a2da5983f
YUSUF EMRE DENİZ <yusufemredenix@outlook.com>
2024-12-26 00:49:26 +0300

    Güncellenmiş css eklenmiş slider sayfası

---
+++ Kurumsal_Web11/Controllers/SliderController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Kurumsal_Web11.Models.DataContext;
+using Kurumsal_Web11.Models.Model;
+
+namespace Kurumsal_Web11.Controllers
+{
+    public class SliderController : Controller
+    {
+        private KurumsalDBContext db = new KurumsalDBContext();
+
+        // GET: Sliders
+        public ActionResult Index()
+        {
+            return View(db.Slider.ToList());
+        }
+
+        // GET: Sliders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // GET: Sliders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sliders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SliderId,Baslik,Acikllama,ResimURL")] Slider slider,HttpPostedFileBase ResimURL)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ResimURL != null)
+                {
+                    WebImage img = new WebImage(ResimURL.InputStream);
+                    FileInfo imginfo = new FileInfo(ResimURL.FileName);
+
+                    string sliderimgname = Guid.NewGuid().ToString() + imginfo.Extension;
+                    img.Resize(1024, 360);
+                    img.Save("~/Uploads/Slider/" + sliderimgname);
+
+                    slider.ResimURL = "/Uploads/Slider/" + sliderimgname;
+                }
+                db.Slider.Add(slider);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(slider);
+        }
+
+        // GET: Sliders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // POST: Sliders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SliderId,Baslik,Acikllama,ResimURL")] Slider slider,HttpPostedFileBase ResimURL,int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var s = db.Slider.Where(x => x.SliderId == id).SingleOrDefault();
+                if (ResimURL != null)
+                {
+                    // Eski resmi sil
+                    string fullPath = Server.MapPath(s.ResimURL);
+                    if (!string.IsNullOrEmpty(s.ResimURL) && System.IO.File.Exists(fullPath))
+                    {
+                        System.IO.File.Delete(fullPath);
+                    }
+                    WebImage img = new WebImage(ResimURL.InputStream);
+                    FileInfo imginfo = new FileInfo(ResimURL.FileName);
+
+                    string sliderimgname = Guid.NewGuid().ToString() + imginfo.Extension;
+                    img.Resize(1024, 360, preserveAspectRatio: true, preventEnlarge: true);
+                    img.Save("~/Uploads/Slider/" + sliderimgname);
+
+                    s.ResimURL = "/Uploads/Slider/" + sliderimgname;
+                }
+
+                s.Baslik = slider.Baslik;
+                s.Acikllama = slider.Acikllama;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(slider);
+        }
+
+        // GET: Sliders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // POST: Sliders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Slider slider = db.Slider.Find(id);
+
+            if (System.IO.File.Exists(Server.MapPath(slider.ResimURL)))
+            {
+                System.IO.File.Delete(Server.MapPath(slider.ResimURL));
+            }
+
+            db.Slider.Remove(slider);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
